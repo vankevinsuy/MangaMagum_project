c62c02c32039e767e087fa6a92f950a88f1a2856
van-kevin suy <33053798+vankevinsuy@users.noreply.github.com>
2019-02-02 17:55:06 +0100

    test merge

--- android/app/src/main/java/com/example/mangamagum/model/DataBase.java
+++ android/app/src/main/java/com/example/mangamagum/model/DataBase.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.support.v7.app.AlertDialog;
-import android.widget.Toast;
 
 public class DataBase extends SQLiteOpenHelper {
 
